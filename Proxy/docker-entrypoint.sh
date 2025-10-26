8661275fdd86a0b51b1d5533771da4f711d2ad39
ldingsol <luisabigaildiaz@gmail.com>
2025-10-26 12:50:47 -0400

    Delete Proxy directory

--- Proxy/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Espera 5 segundos. Esto da tiempo al DNS interno de Docker para estabilizarse.
-echo "Esperando 5 segundos para que la resolución de DNS se estabilice..."
-sleep 5
-
-# Ejecuta el entrypoint original de Nginx
-exec /docker-entrypoint.sh "$@"
